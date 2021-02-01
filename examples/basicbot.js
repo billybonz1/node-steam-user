7f603c0970821b5baf1b42e214c0c9e9023d8f6b
Artem <kalini.art@gmail.com>
2021-02-01 15:43:07 +0200

    New Commit1

--- examples/basicbot.js
+++ examples/basicbot.js
@@ -6,8 +6,6 @@
 var express = require("express");
 var app = express();
 const SteamUser = require('../index.js'); // Replace this with `require('steam-user');` if used outside of the module directory
-var client = new SteamUser();
-
 
 
 var port = process.env.PORT || 5000;
@@ -19,6 +17,7 @@ app.get('/', function(request, response) {
 
 
 	if(request.query.login && request.query.pass){
+		var client = new SteamUser();
 		client.logOn({
 			"accountName": request.query.login,
 			"password": request.query.pass
@@ -26,16 +25,16 @@ app.get('/', function(request, response) {
 
 		// client.httpProxy = "http://184.185.2.103:4145";
 
-		client.on('loggedOn', function(details) {
-			console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
-			client.setPersona(SteamUser.EPersonaState.Online);
-			client.gamesPlayed(440);
-		});
+		// client.on('loggedOn', function(details) {
+		// 	console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
+		// 	client.setPersona(SteamUser.EPersonaState.Online);
+		// 	client.gamesPlayed(440);
+		// });
 
-		client.on('error', function(e) {
-			// Some error occurred during logon
-			console.log(e);
-		});
+		// client.on('error', function(e) {
+		// 	// Some error occurred during logon
+		// 	console.log(e);
+		// });
 
 		client.on('webSession', function(sessionID, cookies) {
 			console.log("Got web session");
@@ -43,54 +42,54 @@ app.get('/', function(request, response) {
 			// Do something with these cookies if you wish
 		});
 
-		client.on('newItems', function(count) {
-			console.log(count + " new items in our inventory");
-		});
+		// client.on('newItems', function(count) {
+		// 	console.log(count + " new items in our inventory");
+		// });
 
-		client.on('emailInfo', function(address, validated) {
-			console.log("Our email address is " + address + " and it's " + (validated ? "validated" : "not validated"));
-		});
+		// client.on('emailInfo', function(address, validated) {
+		// 	console.log("Our email address is " + address + " and it's " + (validated ? "validated" : "not validated"));
+		// });
 
-		client.on('wallet', function(hasWallet, currency, balance) {
-			console.log("Our wallet balance is " + SteamUser.formatCurrency(balance, currency));
-		});
+		// client.on('wallet', function(hasWallet, currency, balance) {
+		// 	console.log("Our wallet balance is " + SteamUser.formatCurrency(balance, currency));
+		// });
 
-		client.on('accountLimitations', function(limited, communityBanned, locked, canInviteFriends) {
-			var limitations = [];
+		// client.on('accountLimitations', function(limited, communityBanned, locked, canInviteFriends) {
+		// 	var limitations = [];
 
-			if (limited) {
-				limitations.push('LIMITED');
-			}
+		// 	if (limited) {
+		// 		limitations.push('LIMITED');
+		// 	}
 
-			if (communityBanned) {
-				limitations.push('COMMUNITY BANNED');
-			}
+		// 	if (communityBanned) {
+		// 		limitations.push('COMMUNITY BANNED');
+		// 	}
 
-			if (locked) {
-				limitations.push('LOCKED');
-			}
+		// 	if (locked) {
+		// 		limitations.push('LOCKED');
+		// 	}
 
-			if (limitations.length === 0) {
-				console.log("Our account has no limitations.");
-			} else {
-				console.log("Our account is " + limitations.join(', ') + ".");
-			}
+		// 	if (limitations.length === 0) {
+		// 		console.log("Our account has no limitations.");
+		// 	} else {
+		// 		console.log("Our account is " + limitations.join(', ') + ".");
+		// 	}
 
-			if (canInviteFriends) {
-				console.log("Our account can invite friends.");
-			}
-		});
+		// 	if (canInviteFriends) {
+		// 		console.log("Our account can invite friends.");
+		// 	}
+		// });
 
-		client.on('vacBans', function(numBans, appids) {
-			console.log("We have " + numBans + " VAC ban" + (numBans == 1 ? '' : 's') + ".");
-			if (appids.length > 0) {
-				console.log("We are VAC banned from apps: " + appids.join(', '));
-			}
-		});
+		// client.on('vacBans', function(numBans, appids) {
+		// 	console.log("We have " + numBans + " VAC ban" + (numBans == 1 ? '' : 's') + ".");
+		// 	if (appids.length > 0) {
+		// 		console.log("We are VAC banned from apps: " + appids.join(', '));
+		// 	}
+		// });
 
-		client.on('licenses', function(licenses) {
-			console.log("Our account owns " + licenses.length + " license" + (licenses.length == 1 ? '' : 's') + ".");
-		});
+		// client.on('licenses', function(licenses) {
+		// 	console.log("Our account owns " + licenses.length + " license" + (licenses.length == 1 ? '' : 's') + ".");
+		// });
 	}else{
 		response.send(request.query);
 	}
